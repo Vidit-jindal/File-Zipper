1e73cec7394f5b365901d595698bef8627df2011
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-12-03 00:08:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 import { HuffmanCoder } from './huffman.js';
+
+onload = function () {
+    // Get reference to elements
+    const treearea = document.getElementById('treearea');
+    const encode = document.getElementById('encode');
+    const decode = document.getElementById('decode');
+    const temptext = document.getElementById('temptext');
+    const upload = document.getElementById('uploadedFile');
