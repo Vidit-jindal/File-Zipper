dd1de9050a6ed1d923521daf5632a00c2d071c70
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-12-20 00:54:47 +0530

    Update huffman.js

--- huffman.js
+++ huffman.js
@@ -7,3 +7,6 @@ class HuffmanCoder{
         if(typeof(node[1])==="string"){
             return '\''+node[1];
         }
+        
+        return '0' + this.stringify(node[1][0]) + '1' + this.stringify(node[1][1]);
+    }
