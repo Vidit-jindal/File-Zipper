faaeedcb6f03352612d0131e5a0eb9683c2f0193
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-12-20 00:54:37 +0530

    Update huffman.js

--- huffman.js
+++ huffman.js
@@ -1,3 +1,9 @@
 import { BinaryHeap } from './heap.js';
 
 export { HuffmanCoder }
+class HuffmanCoder{
+
+    stringify(node){
+        if(typeof(node[1])==="string"){
+            return '\''+node[1];
+        }
