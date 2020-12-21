cc37644da81ea8e667d365705b1efe3702150b90
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-12-22 01:23:07 +0530

    Update huffman.js

--- huffman.js
+++ huffman.js
@@ -16,3 +16,12 @@ class HuffmanCoder{
             if(node[1].length===1)
                 node[1] = node[1][0];
         }
+        if(typeof(node[1])==="string"){
+            return String(index) + " = " + node[1];
+        }
+
+        let left = this.display(node[1][0], modify, index*2);
+        let right = this.display(node[1][1], modify, index*2+1);
+        let res = String(index*2)+" <= "+index+" => "+String(index*2+1);
+        return res + '\n' + left + '\n' + right;
+    }
